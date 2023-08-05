a0b64a20cd4e0442804257cf78e8732a871a2881
Luis Mendoza <hyh@hotmail.com>
2023-08-05 10:41:37 -0500

    Footer
    Se realizar vista del componente footer

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LandingPageRoutingModule } from './landing-page-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LayoutLandingPageComponent } from './layout/layout-landing-page/layout-landing-page.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
-    LayoutLandingPageComponent
+    LayoutLandingPageComponent,
+    FooterComponent
 
   ],
   imports: [
